a0e98f6bac44043a2615b1f1b37b6a82ce3cf6f3
Jose Silva <j.alberto.silva23@gmail.com>
2022-01-21 22:37:49 +0000

    One embed at a time

--- script.js
+++ script.js
@@ -2,14 +2,7 @@ window.addEventListener('DOMContentLoaded',function (){
   var aboutSection =this.document.getElementById('about'),
       mainSection= this.document.querySelector('.page-content'),
       aboutButton=this.document.querySelector('.about-button'),
-      tabButtons=this.document.querySelectorAll('.tab-selector'),
-      tabContent=this.document.querySelectorAll('.weather-card');
-
-  window.addEventListener('click', (e) =>{
-  
-      console.log(e.target);
-  
-  })
+      tabButtons=this.document.querySelectorAll('.tab-selector');
       
   aboutButton.addEventListener('click', (event) =>{
       mainSection.classList.toggle('blurred');
@@ -31,14 +24,21 @@ window.addEventListener('DOMContentLoaded',function (){
     })    
   });
 
-  function changeTab(event, tabName){
-    tabContent.forEach(element => {
-      element.classList.remove('active');
-      if(element.classList.contains(tabName)){
-        element.classList.toggle('active');       
-        event.target.classList.toggle('active');
-      }    
-    });
+  function changeTab(e, tabName){
+    let div=document.querySelector('.tab-content');
+    let idsList= {'sunny':'5SosqlxeVmbp47asMuQlqi', 'cloudy':'3nR8btC3svZmUktrXDBuRY', 'rainy':'2HC4JdZb7ZqFto0xX4OkMv', 'storm':'4bJCKmpKgti10f3ltz6LLl'};
+    const iFrame= document.createElement('iframe');
+    iFrame.setAttribute('src', `https://open.spotify.com/embed/album/${idsList[tabName]}?utm_source=generator`);
+    iFrame.setAttribute('frameBorder', "0");
+    iFrame.setAttribute('allow','autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture');
+    iFrame.style.display="none";
+    iFrame.classList.add('weather-canvas');
+    iFrame.addEventListener('load', () =>{
+      if(div.querySelectorAll('.weather-canvas').length>1) div.removeChild(div.querySelectorAll('.weather-canvas')[0]);
+      iFrame.style.display="block";
+      e.target.classList.toggle('active');
+    })
+    div.appendChild(iFrame);
   }
 
 });
